1efa11b2faa8563c952b7d48b89a7aee155717f2
Daniel Varab <djam@itu.dk>
2014-09-05 20:36:00 +0200

    living hell

--- assignments/Mini Project 1/src/TCPForwarder.java
+++ assignments/Mini Project 1/src/TCPForwarder.java
@@ -1,43 +1,60 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
 
 public class TCPForwarder implements Runnable {
     private String h = "";
     private int p1, p2;
 
     public static void main(String[] args) {
-        UDPForwarder first = new UDPForwarder("TESTING!", 1234, 4321);
-        UDPForwarder second = new UDPForwarder("TESTING!", 4321, 8888);
-        new Thread(first);
-        new Thread(second);
+        new Thread(new TCPForwarder(4820));
     }
 
-    public TCPForwarder(String h, int p1, int p2) {
-        this.h = h;
+    public TCPForwarder(int p1) {
         this.p1 = p1;
-        this.p2 = p2;
         run();
     }
 
     @Override
     public void run() {
+    	ServerSocket socket;
+    	Socket connection;
         try{
-            ServerSocket socket = new ServerSocket(p1);
-            Socket forwarder = new Socket("localhost", p2);
-            byte[] buffer = new byte[1000];
-
+            socket = new ServerSocket(p1);
             while(true){
-                Socket connection = socket.accept();
-                BufferedReader received = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                DataOutputStream output = new DataOutputStream(forwarder.getOutputStream());
-                System.out.println("Listening . . .");
-                output.writeBytes(received.readLine());
+            	connection = socket.accept();
+            	BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            	DataOutputStream outToClient = new DataOutputStream(connection.getOutputStream());
+            	String clientSentence = "";
+            	String result = "";
+            	
+            	while((clientSentence = inFromClient.readLine()).length() != 0) {
+            		result += clientSentence + "\n";
+            	}
+            	System.out.println("Client data retrieved.");
+            	//outToClient.writeBytes(result);
+            	//outToClient.close();
+             	//inFromClient.close();
+             	
+            	// retrieve data from ITU
+                Socket s = new Socket("google.com", 80);    
+                Scanner in = new Scanner(s.getInputStream());
+                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+                
+                System.out.println("Sended: \n" + result);
+                // fuckign send it!
+
+                connection.close();
+                s.close();             	
             }
         } catch(Exception e){
-            System.out.println("You suck!");
+            e.printStackTrace();
         }
     }
 }

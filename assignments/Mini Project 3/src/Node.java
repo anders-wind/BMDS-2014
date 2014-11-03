467ac81d6d390510366b47be1befdcd291149cc9
Anders Fischer-Nielsen <Anders@Anderss-MacBook-Pro.local>
2014-11-03 12:33:25 +0100

    Oprettede tomme klasser. 👍

---
+++ assignments/Mini Project 3/src/Node.java
@@ -0,0 +1,18 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by Anders on 03/11/14.
+ */
+public class Node {
+    public Node(String ownIp, int ownKey) {
+
+    }
+
+    public String get(int messageKey, String otherNodeIp, int ownPort) {
+        throw new NotImplementedException();
+    }
+
+    public void put(int messageKey, String valueToPut) {
+
+    }
+}

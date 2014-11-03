6fdcac5ec98ad8b6119f37d7cc83907ac66c83ec
Anders Fischer-Nielsen <Anders@Anderss-MacBook-Pro.local>
2014-11-03 12:09:40 +0100

    Oprettede tomme klasser. 👍

---
+++ assignments/Mini Project 3/src/Get.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Anders on 03/11/14.
+ */
+public class Get {
+
+}
---
+++ assignments/Mini Project 3/src/Put.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Anders on 03/11/14.
+ */
+public class Put {
+
+    public Put(String IP, int key, String value) {
+
+    }
+
+    public void put(int key, String value) {
+
+    }
+}
